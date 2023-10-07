e49d59c7ac971483c26a9aa541adedcef9810055
nam <namnv456@gmail.com>
2023-10-07 15:14:20 +0700

    init

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,6 +14,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final Dio dio = Dio();
   final service = APIService();
+  int currentIndex = 0;
+  bool isLightOn = false; // Track the state of the light
 
   Future<void> _toggleTheme() async {
     final settings = Provider.of<ThemeSettings>(context, listen: false);
@@ -29,27 +31,67 @@ class _MyHomePageState extends State<MyHomePage> {
       }
     } catch (error) {}
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ClipOval(
-          child: Material(
-            color: Colors.amber, // Background color
-            child: InkWell(
-              onTap: _toggleTheme,
-              child: const SizedBox(
-                width: 100.0, // Adjust the width and height as needed
-                height: 100.0,
-                child: Icon(
-                  Icons.lightbulb_sharp,
-                  size: 60.0, // Adjust the icon size as needed
-                  color: Colors.white, // Icon color
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+/*
+            if (currentIndex == 1) // Only show the light control when "Lights" tab is selected
+*/
+              ClipOval(
+                child: Material(
+                  color: isLightOn ? Colors.yellow : Colors.grey, // Light on/off color
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        isLightOn = !isLightOn;
+                      });
+                    },
+                    child: const SizedBox(
+                      width: 100.0, // Adjust the width and height as needed
+                      height: 100.0,
+                      child: Icon(
+                        Icons.lightbulb_sharp,
+                        size: 60.0, // Adjust the icon size as needed
+                        color: Colors.black, // Icon color
+                      ),
+                    ),
+                  ),
                 ),
               ),
+            const SizedBox(height: 16.0),
+            Text(
+              isLightOn ? 'Light is ON' : 'Light is OFF',
+              style: TextStyle(fontSize: 18.0),
             ),
-          ),
+          ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.wb_cloudy),
+            label: 'Weather',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.lightbulb_sharp),
+            label: 'Lights',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.timer),
+            label: 'Timer',
+          ),
+        ],
       ),
     );
   }
