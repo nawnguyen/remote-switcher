386ccab2d0876af76a2a03ec6359181d4282bf60
nam <namnv456@gmail.com>
2023-10-28 11:37:22 +0700

    feat: add switch and delete switch

--- lib/features/switch/presentation/light_switch.dart
+++ lib/features/switch/presentation/light_switch.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:light_switch_app/features/core/application/routes/route_names.dart';
-import 'package:light_switch_app/services/api_service.dart';
-import 'package:provider/provider.dart';
-import '../../../provider/theme_settings.dart';
 
 class SwitchPage extends StatefulWidget {
   const SwitchPage({Key? key}) : super(key: key);
@@ -12,71 +7,139 @@ class SwitchPage extends StatefulWidget {
   State<SwitchPage> createState() => _SwitchPageState();
 }
 
+class SwitchModel {
+  final String name;
+  bool state;
+
+  SwitchModel({required this.name, required this.state});
+}
+
 class _SwitchPageState extends State<SwitchPage> {
-  final service = APIService();
-  int currentIndex = 0;
-  bool isLightOn = false; // Track the state of the light
-
-  Future<void> _toggleTheme() async {
-    final settings = Provider.of<ThemeSettings>(context, listen: false);
-    settings.toggleTheme();
-
-    await Future.delayed(const Duration(milliseconds: 1));
-
-    try {
-      if (settings.isDark) {
-        service.toggle('off');
-      } else {
-        service.toggle('on');
-      }
-    } catch (error) {}
-  }
+  List<SwitchModel> switches = [
+    SwitchModel(name: 'on', state: true),
+    SwitchModel(name: 'off', state: false),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-/*
-            if (currentIndex == 1) // Only show the light control when "Lights" tab is selected
-*/
-              ClipOval(
-                child: Material(
-                  color: isLightOn ? Colors.yellow : Colors.grey, // Light on/off color
-                  child: InkWell(
-                    onTap: () {
-                      setState(() {
-                        isLightOn = !isLightOn;
-                      });
-                    },
-                    child: const SizedBox(
-                      width: 100.0, // Adjust the width and height as needed
-                      height: 100.0,
-                      child: Icon(
-                        Icons.lightbulb_sharp,
-                        size: 60.0, // Adjust the icon size as needed
-                        color: Colors.black, // Icon color
-                      ),
-                    ),
+      appBar: AppBar(
+        backgroundColor: Colors.black38,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add_circle, size: 40),
+            onPressed: () {
+              _showBottomSheet(context);
+            },
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
+          ),
+          itemCount: switches.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onLongPress: () {
+                _showDeleteBottomSheet(context, index);
+              },
+              onTap: () {
+                setState(() {
+                  switches[index].state = !switches[index].state;
+                });
+              },
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: switches[index].state ? Colors.yellow : Colors.black26,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
                   ),
                 ),
+                child: Center(
+                  child: Text(switches[index].name),
+                ),
               ),
-            const SizedBox(height: 16.0),
-            Text(
-              isLightOn ? 'Light is ON' : 'Light is OFF',
-              style: const TextStyle(fontSize: 18.0),
-            ),
-          ],
+            );
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.goNamed(RouteNames.timerSettingsNameRoute);
-        },
-        child: const Icon(Icons.timer),
-      ),
+    );
+  }
+
+  void _showBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              ListTile(
+                title: TextField(
+                  decoration: const InputDecoration(labelText: 'Enter Name'),
+                  onSubmitted: (value) {
+                    setState(() {
+                      final switchModel = SwitchModel(name: value, state: false);
+                      switches.add(switchModel);
+                    });
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (switches.isEmpty) {
+                    // switches.add("New Name");
+                  }
+                  Navigator.pop(context);
+                },
+                child: const Text('Save'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _showDeleteBottomSheet(BuildContext context, int index) {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          child: ListTile(
+            title: Text('Delete'),
+            onTap: () {
+              setState(() {
+                switches.removeAt(index);
+              });
+              Navigator.pop(context);
+            },
+          ),
+        );
+      },
     );
   }
 }
